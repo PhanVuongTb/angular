ba6d81a8046bd8445c3f3f2560fb798641576440
Phan Vuong <pducvuongtb@gamil.com>
2022-05-10 09:02:10 +0700

    buoi 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeloComponent } from '../hello/hello.component';
+import { HeloComponent } from './hello/hello.component';
+import { HelloComponent } from './components/hello/hello.component';
+import { Hello2Component } from './components/hello2/hello2.component';
+import { ProductsComponent } from './Page/products/products.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeloComponent
+    HeloComponent,
+    HelloComponent,
+    Hello2Component,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
